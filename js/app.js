52d082bbaedd3c47704166249d4a22409090ca6d
Pei-Hwa Wu <summer.wph@gmail.com>
2018-09-18 12:34:37 -0700

    Updated coment and coding style

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+let Enemy = function(x, y, speed) {
+
     // The enemies position and move speed
     this.x = x;
     this.y = y;
@@ -13,26 +14,23 @@ var Enemy = function(x, y, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
     this.x += this.speed * dt;
-    // console.log('Enemies position: ' + this.x);
 
     // Checking if the emeny out of the border and the emeny cross the screen.
-    if (this.x > 505){
+    if (this.x > 505) {
       this.x = -100;
     }
 
     //Checking if the collision with player
-    for (let enemy of allEnemies){
+    for (let enemy of allEnemies) {
       enemy.checkCollisions(player);
     }
 };
 
-Enemy.prototype.checkCollisions = function(p){
-  // console.log("Enemy: " + this.width + ',' + this.height + ',' + this.x + ',' + this.y);
-  // console.log("Player: " + p.width + ',' + p.height + ',' + p.x + ',' + p.y);
+Enemy.prototype.checkCollisions = function(p) {
+
+    // Using the area matching to detect collision
     if (this.x < p.x + p.width  && this.x + this.width  > p.x &&
       this.y < p.y + p.height && this.y + this.height > p.y) {
         // The emeny-player collision, reset the game.
@@ -48,7 +46,7 @@ Enemy.prototype.render = function() {
 
 
 // Player's cless
-var Player = function(x, y, speed) {
+let Player = function(x, y, speed) {
   this.x = x;
   this.y = y;
   this.height = 30;
@@ -58,22 +56,21 @@ var Player = function(x, y, speed) {
 
 }
 
-// Update Player's position
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
+// Update Player's position to check if the player moves off screen or wins the game.
+Player.prototype.update = function() {
 
     // Player cannot move off screen
     // Checking if the player touch the border(left, right and bottm walls)
-    if (this.x >= 410){
+    if (this.x >= 410) {
       this.x = 410;
-    }else if (this.x <= 0){
+    }else if (this.x <= 0) {
       this.x = 0;
-    }else if (this.y >= 430){
+    }else if (this.y >= 430) {
       this.y = 430;
     }
 
     // Checking if the player touch the water area or top wall to win the game.
-    if (this.y <=50){
+    if (this.y <=50) {
       this.y = -20;
     }
 };
@@ -101,16 +98,16 @@ Player.prototype.handleInput = function(dt) {
   }
 };
 
-var allEnemies = [];
-var enemy = new Enemy(-100, Math.random() * 184 + 50, Math.random() * 256);
-var player = new Player(202, 405);
+let allEnemies = [];
+let enemy = new Enemy(-100, Math.random() * 184 + 50, Math.random() * 256);
+let player = new Player(202, 405);
 allEnemies.push(enemy);
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
